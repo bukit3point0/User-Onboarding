cc2e6bee47607192086c67d570562850e638feec
bukit3point0 <bukit3point0@gmail.com>
2021-02-24 00:40:16 -0500

    getting the layout done to implement js tomorrow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-
+import React from 'react'
 import './App.css';
 import Form from './Form'
+import styled from 'styled-components'
+import background from './images/matrix-rain.jpg'
+
+const LoginForm = styled.div``
+const HeaderContainer = styled.div``
+const Welcome = styled.h1`
+  text-align: center;
+  font-family: 'DotGothic16', sans-serif;
+  font-size: 3rem;
+  letter-spacing: 10px;
+  width: 80%;
+  margin: 2% auto;
+`
 
 function App() {
   return (
-    <div>
+    <LoginForm>
+      <HeaderContainer>
+      <Welcome>Welcome. Are you the one we've been waiting for?</Welcome>
+      </HeaderContainer>
       <Form/>
-    </div>
+    </LoginForm>
   );
 }
 
--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const FormContainer = styled.div`
+font-family: 'DotGothic16', sans-serif;
+    border: 1px solid black;
+    width: 50%;
+    margin: auto;
+    font-size:2rem;
+    outline: 1px solid black;
+`
+
+const FillBox = styled.div`
+    padding: 1rem;
+`
+
+const Line = styled.div`
+    padding: 0 1rem;
+`
+
+const FormName = styled.label`
+    margin: 1rem 0;
+`
+
+const Input = styled.input`
+    margin-top: 1rem;
+    margin-left: 1rem;
+    width: 50%;
+`
+
+const Check = styled.input`
+    margin: 1rem;
+`
+
+const GoDownTheRabbitHole = styled.button`
+    display: block;
+    margin: 0 auto 2rem;
+    background-color: red;
+    font-weight: bold;
+`
 
 const Form = () => {
 
     return (
-        <div>
-
-        </div>
+        <FormContainer>
+            <FillBox>
+                <Line>
+                    <FormName>Name:</FormName>
+                    <Input name="name" type="name"/>
+                </Line>
+                <Line>
+                    <FormName>Email Address:</FormName>
+                    <Input name="email" type="email"/>
+                </Line>
+                <Line>
+                    <FormName>Password:</FormName>
+                    <Input name="password" type="password"/>
+                </Line>
+                <Line>
+                    <Check name="TOS" type="checkbox"/>
+                    <FormName>I'm ready to go down the rabbit hole. I'm taking the red pill.</FormName>
+                </Line>
+            </FillBox>
+            <GoDownTheRabbitHole>I'm ready, Morpheus.</GoDownTheRabbitHole>
+        </FormContainer>
     )
 }
 
